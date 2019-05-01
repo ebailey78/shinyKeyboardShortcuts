c09b3f1ef7fb1ca33f6bada6283eec66f4f50f86
Eric <ebailey78@users.noreply.github.com>
2019-05-01 13:19:53 -0400

    Update old_faithful.R

--- inst/examples/old_faithful.R
+++ inst/examples/old_faithful.R
@@ -6,6 +6,89 @@ ui <- fluidPage(
 
     # Application title
     titlePanel("Old Faithful Geyser Data"),
+    tags$style("
+
+      key {
+        display: inline-block;
+        font-family: monospace;
+        font-weight: bold;
+        box-sizing: border-box;
+        border: solid #333 2px;
+        text-align: center;
+        min-width: 2em;
+        min-height: 2em;
+        padding: 0.5em;
+        margin: 0.25em;
+        background-color: #F6F6F6;
+        color: #333;
+        border-radius: 5px;
+        box-shadow: 0.25em 0.25em;
+      }
+      
+      key.pressed {
+      
+        background-color: #7FDBFF;
+        box-shadow: 0 0;
+        transform: translate(0.25em, 0.25em);
+      }
+
+      shortcut {
+        display: block;
+        font-family: sans-serif;
+        font-weight: bold;
+        color: #333;
+      }
+      
+      shortcut.lg {
+        font-size: 30px;
+      }
+      
+      shortcut.sm {
+        font-size: 10px;
+      }
+           
+      shortcut.sm > key {
+        border: solid #333 1.25px;
+      }
+      
+      shortcut > description {
+      
+        font-size: 1.2em;
+      
+      }
+      
+      shortcut > description::before {
+        content: ":";
+        padding: 3px;
+      }
+      
+"),
+    tags$script("
+      $(document).on('keydown', function(e) {
+      
+        let key = e.originalEvent.key.toLowerCase();
+        
+        $('key').each(function() {
+          if($(this).data('key') == key) {
+            $(this).addClass("pressed");
+          }
+        });
+      
+      });
+    
+      $(document).on('keyup', function(e) {
+      
+        let key = e.originalEvent.key.toLowerCase();
+        
+        $('key').each(function() {
+          if($(this).data('key') == key) {
+            $(this).removeClass("pressed");
+          }
+        });
+      
+      });
+
+"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
@@ -15,9 +98,20 @@ ui <- fluidPage(
                         min = 1,
                         max = 50,
                         value = 30),
-            tags$p("Alt+Shift+a: Decrease Bin Count"),
-            tags$p("Alt+Shift+q: Increase Bin Count"),
-            tags$p("Alt+Shift+r: Reset Bin Count"),
+            HTML("
+    <shortcut class = 'sm'>
+      <key data-key = 'control'>Ctrl</key>+<key data-key='alt'>Alt</key>+<key data-key = 'a'>A</key>
+      <description>Increase Bin Size</description>
+    </shortcut>
+    <shortcut class = 'sm'>
+      <key data-key = 'control'>Ctrl</key>+<key data-key='alt'>Alt</key>+<key data-key = 'q'>Q</key>
+      <description>Decrease Bin Size</description>
+    </shortcut>
+    <shortcut class = 'sm'>
+      <key data-key = 'control'>Ctrl</key>+<key data-key='alt'>Alt</key>+<key data-key = 'r'>R</key>
+      <description>Reset Bin Size</description>
+    </shortcut>  
+            "),
             keyboardShortcut('bins_down', key = 'a', alt = TRUE, shift = TRUE),
             keyboardShortcut('bins_up', key = 'q', alt = TRUE, shift = TRUE),
             keyboardShortcut('reset_bins', key = 'r', alt = TRUE, shift = TRUE)
